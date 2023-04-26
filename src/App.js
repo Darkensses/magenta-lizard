420eae6489b3e9786d041366c4fa77d2a7affe20
Jasiel Guillén <darkensses@gmail.com>
2023-04-26 00:59:07 -0600

    :sparkles: First Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useSpring, animated, config } from '@react-spring/three'
+import { useRef, useState } from 'react';
+import targetMind01 from "./test1.mind";
+import targetMulti from "./testMulti.mind";
+import { RobotPlayground } from './models/RobotPlayground';
+import { ToyDinoAllosaurus } from './models/ToyDinoAllosaurus';
+import { EffectComposer, DepthOfField, Bloom, Noise, Vignette } from '@react-three/postprocessing'
+import { ARAnchor, ARView } from './components/AR';
+import { Particles } from './FX';
+
+function Box() {
+  let refBox = useRef();
+  const [active, setActive] = useState(false)
+  const { scaleValue } = useSpring({
+    scaleValue: active ? 1.5 : 1,
+    config: config.slow
+  })
+
+  useFrame( () => {
+    refBox.current.rotation.x += 0.01;
+    refBox.current.rotation.y += 0.01;
+  });
+
+  return(
+    <animated.mesh ref={refBox} scale={scaleValue} onPointerOver={e => setActive(true)} onPointerOut={e => setActive(false)}>
+      <boxGeometry args={[1,1,1]} />
+      <meshMatcapMaterial color={0xff00ff} />
+    </animated.mesh>
+  )
+}
+
+function App2() {  
+  return (
+    <div className="App">
+      <p style={{position: 'absolute'}}>Magenta Lizard 🌸🐊XD</p>
+      
+      <ARView
+        autoplay
+        flipUserCamera={false}
+        imageTargets={targetMind01}
+        filterMinCF={0.0001}
+        filterBeta={0.001} // Speed Coefficient, increase to reduce delay
+        warmupTolerance={0} // Number of continuous frames required for a target being detected to be marked as found
+        missTolerance={5} // Number of continuous frames required for a target not being detected to be marked as lost
+        camera={{fov: 75, near: 0.1, far: 1000, position: [0, 0, 0]}}
+      >
+        <EffectComposer>
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+        </EffectComposer>
+        <directionalLight />
+        <ARAnchor target={0}>
+          <ToyDinoAllosaurus/>          
+        </ARAnchor>          
+      </ARView>
+    </div>
+  );
+}
+
+function AppTest() {
+  return(
+    <div className="App">
+      <p style={{position: 'absolute', zIndex: 1}}>Magenta Lizard 🌸🐊XD</p>
+      <Canvas style={{ background: "hotpink" }}>
+        <Box />
+      </Canvas>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p style={{position: 'absolute', bottom:100, left: "50%"}}>Magenta Lizard 🌸🐊XD</p>
+      <div style={{position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', left:0,right:0,top:0,bottom:0}}>
+        <div style={{width: "80vw", height:"80vh"}}>
+          <div className='inner'>
+            <div className='scanline'></div>
+          </div>
+        </div>
+      </div>
+      
+      <ARView
+        autoplay
+        flipUserCamera={false}
+        imageTargets={targetMulti}
+        maxTrack={3}
+        filterMinCF={0.0001}
+        filterBeta={0.001} // Speed Coefficient, increase to reduce delay
+        warmupTolerance={0} // Number of continuous frames required for a target being detected to be marked as found
+        missTolerance={5} // Number of continuous frames required for a target not being detected to be marked as lost
+        camera={{fov: 75, near: 0.1, far: 1000, position: [0, 0, 0]}}
+      >
+        
+        <directionalLight />
+        <ARAnchor target={0}>                      
+            <RobotPlayground />
+        </ARAnchor> 
+
+        <ARAnchor target={1}>                      
+            <Box/>       
+        </ARAnchor> 
+
+        <ARAnchor target={2}>                      
+            <ToyDinoAllosaurus/>
+            <Particles count={200}/>        
+        </ARAnchor> 
+
+                  
+      </ARView>
     </div>
   );
 }
---
+++ src/FX/index.js
@@ -0,0 +1,182 @@
+import * as THREE from 'three'
+import React, { useRef, useMemo } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
+
+function Swarm({ count }) {
+  const mesh = useRef()
+  const light = useRef()
+  const { viewport, mouse } = useThree()
+
+  const dummy = useMemo(() => new THREE.Object3D(), [])
+  // Generate some random positions, speed factors and timings
+  const particles = useMemo(() => {
+    const temp = []
+    for (let i = 0; i < count; i++) {
+      const t = Math.random() * 100
+      const factor = 20 + Math.random() * 100
+      const speed = 0.01 + Math.random() / 200
+      const xFactor = -50 + Math.random() * 100
+      const yFactor = -50 + Math.random() * 100
+      const zFactor = -50 + Math.random() * 100
+      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 })
+    }
+    return temp
+  }, [count])
+  // The innards of this hook will run every frame
+  useFrame((state) => {
+    // Makes the light follow the mouse
+    light.current.position.set((mouse.x * viewport.width) / 2, (mouse.y * viewport.height) / 2, 0)
+    // Run through the randomized data to calculate some movement
+    particles.forEach((particle, i) => {
+      let { t, factor, speed, xFactor, yFactor, zFactor } = particle
+      // There is no sense or reason to any of this, just messing around with trigonometric functions
+      t = particle.t += speed / 2
+      const a = Math.cos(t) + Math.sin(t * 1) / 10
+      const b = Math.sin(t) + Math.cos(t * 2) / 10
+      const s = Math.cos(t)
+      particle.mx += mouse.x * viewport.width * particle.mx * 0.01
+      particle.my += mouse.y * viewport.height * particle.my * 0.01
+      // Update the dummy object
+      dummy.position.set(
+        (particle.mx / 10) * a + xFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 1) * factor) / 10,
+        (particle.my / 10) * b + yFactor + Math.sin((t / 10) * factor) + (Math.cos(t * 2) * factor) / 10,
+        (particle.my / 10) * b + zFactor + Math.cos((t / 10) * factor) + (Math.sin(t * 3) * factor) / 10
+      )
+      dummy.scale.set(s, s, s)
+      dummy.rotation.set(s * 5, s * 5, s * 5)
+      dummy.updateMatrix()
+      // And apply the matrix to the instanced item
+      mesh.current.setMatrixAt(i, dummy.matrix)
+    })
+    mesh.current.instanceMatrix.needsUpdate = true
+  })
+  return (
+    <>
+      <pointLight ref={light} distance={60} intensity={20} color="red">
+        <mesh scale={[4, 4, 40]}>
+          <dodecahedronGeometry />
+        </mesh>
+      </pointLight>
+      <instancedMesh ref={mesh} args={[null, null, count]}>
+        <dodecahedronGeometry args={[1, 0]} />
+        <meshStandardMaterial color="black" />
+      </instancedMesh>
+    </>
+  )
+}
+
+const particlesFragmentShader = `
+varying float vDistance;
+
+void main() {
+  vec3 color = vec3(0.34, 0.53, 0.96);
+  float strength = distance(gl_PointCoord, vec2(0.5));
+  strength = 1.0 - strength;
+  strength = pow(strength, 3.0);
+
+  color = mix(color, vec3(0.97, 0.70, 0.45), vDistance * 0.5);
+  color = mix(vec3(0.0), color, strength);
+  gl_FragColor = vec4(color, strength);
+}
+`
+const particlesVertexShader = `
+uniform float uTime;
+uniform float uRadius;
+
+varying float vDistance;
+
+// Source: https://github.com/dmnsgn/glsl-rotate/blob/main/rotation-3d-y.glsl.js
+mat3 rotation3dY(float angle) {
+  float s = sin(angle);
+  float c = cos(angle);
+  return mat3(
+    c, 0.0, -s,
+    0.0, 1.0, 0.0,
+    s, 0.0, c
+  );
+}
+
+
+void main() {
+  float distanceFactor = pow(uRadius - distance(position, vec3(0.0)), 1.5);
+  float size = distanceFactor * 10.0 + 10.0;
+  vec3 particlePosition = position * rotation3dY(uTime * 0.3 * distanceFactor);
+
+  vDistance = distanceFactor;
+
+  vec4 modelPosition = modelMatrix * vec4(particlePosition, 1.0);
+  vec4 viewPosition = viewMatrix * modelPosition;
+  vec4 projectedPosition = projectionMatrix * viewPosition;
+
+  gl_Position = projectedPosition;
+
+  gl_PointSize = size;
+  // Size attenuation;
+  gl_PointSize *= (40000.0 / - viewPosition.z);
+}
+
+`
+
+const Particles = (props) => {
+    const { count } = props;
+    const radius = 2;
+  
+    // This reference gives us direct access to our points
+    const points = useRef();
+  
+    // Generate our positions attributes array
+    const particlesPosition = useMemo(() => {
+      const positions = new Float32Array(count * 3);
+      
+      for (let i = 0; i < count; i++) {
+        const distance = Math.sqrt(Math.random()) * radius;
+        const theta = THREE.MathUtils.randFloatSpread(360); 
+        const phi = THREE.MathUtils.randFloatSpread(360); 
+  
+        let x = distance * Math.sin(theta) * Math.cos(phi)/2.0;
+        let y = distance * Math.sin(theta) * Math.sin(phi)/2.0;
+        let z = distance * Math.cos(theta);
+  
+        positions.set([x, y, z], i * 3);
+      }
+      
+      return positions;
+    }, [count]);
+  
+    const uniforms = useMemo(() => ({
+      uTime: {
+        value: 0.0
+      },
+      uRadius: {
+        value: radius
+      }
+    }), [])
+  
+    useFrame((state) => {
+      const { clock } = state;
+  
+      points.current.material.uniforms.uTime.value = clock.elapsedTime;
+    });
+  
+    return (
+      <points ref={points}>
+        <bufferGeometry>
+          <bufferAttribute
+            attach="attributes-position"
+            count={particlesPosition.length / 3}
+            array={particlesPosition}
+            itemSize={3}
+          />
+        </bufferGeometry>
+        <shaderMaterial
+          blending={THREE.AdditiveBlending}
+          depthWrite={false}
+          fragmentShader={particlesFragmentShader}
+          vertexShader={particlesVertexShader}
+          uniforms={uniforms}
+        />
+      </points>
+    );
+  };
+
+export { Swarm, Particles };
